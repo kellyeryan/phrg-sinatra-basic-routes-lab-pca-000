8399dde75acc7440cfe45cffdc16350de038e33e
kellyeryan <kellyeryan@gmail.com>
2019-10-15 10:54:57 -0400

    Finish sinatra basic routes lab

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is ______"
+  end
+
+  get "/hometown" do
+    "My hometown is _____"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is _______"
+  end
 end
